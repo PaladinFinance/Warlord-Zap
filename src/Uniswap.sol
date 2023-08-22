a1a5307623ca6752013fb1c54e2626c2a16bed37
Alberto <albicento.ac@gmail.com>
2023-08-22 17:20:48 +0200

    feat: Added initial uniswap integration

---
+++ src/Uniswap.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+pragma abicoder v2;
+
+import {ISwapRouter} from "uniswap/ISwapRouter.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+
+contract Uniswap {
+    using SafeTransferLib for ERC20;
+
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    ISwapRouter public immutable swapRouter;
+    uint24 public constant poolFee = 500;
+
+    constructor(ISwapRouter _swapRouter) {
+        swapRouter = _swapRouter;
+    }
+
+    function _approveSwapper(address token) external {// TODO internalize 
+        ERC20(token).safeApprove(address(swapRouter), type(uint256).max);
+    }
+
+    // TODO should the swap router have a setter?
+    // TODO make setter for fee
+
+    function _etherize(address token, uint256 amountIn, uint256 etherOutMin, address receiver)
+        external //  TODO change this
+        returns (uint256 amountOut)
+    {
+        // Pull the specified amount to this contract.
+        ERC20(token).safeTransferFrom(msg.sender, address(this), amountIn);
+
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            tokenIn: token, // The input token address
+            tokenOut: WETH, // The token received should be Wrapped Ether
+            fee: poolFee, // The fee tier of the pool
+            recipient: receiver, // Receiver of the swapped tokens
+            deadline: block.timestamp, // Swap has to be terminated at block time
+            amountIn: amountIn, // The exact amount to swap
+            amountOutMinimum: etherOutMin, // Quote is given by frontend to ensure slippage is minimised
+            sqrtPriceLimitX96: 0 // Ensure we swap our exact input amount.
+        });
+
+        amountOut = swapRouter.exactInputSingle(params);
+    }
+}
