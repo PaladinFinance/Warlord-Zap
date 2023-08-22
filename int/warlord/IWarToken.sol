074bcc155bdf43d6d9e5bc9627d51b2477b5854a
Alberto <albicento.ac@gmail.com>
2023-08-22 16:10:00 +0200

    feat: Added warlord interfaces

---
+++ int/warlord/IWarToken.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.8.10;
+
+interface WarToken {
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+    event NewPendingOwner(address indexed previousPendingOwner, address indexed newPendingOwner);
+    event RoleAdminChanged(bytes32 indexed role, bytes32 indexed previousAdminRole, bytes32 indexed newAdminRole);
+    event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender);
+    event RoleRevoked(bytes32 indexed role, address indexed account, address indexed sender);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    function BURNER_ROLE() external view returns (bytes32);
+    function DEFAULT_ADMIN_ROLE() external view returns (bytes32);
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+    function MINTER_ROLE() external view returns (bytes32);
+    function acceptOwnership() external;
+    function allowance(address, address) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function balanceOf(address) external view returns (uint256);
+    function burn(address from, uint256 amount) external;
+    function decimals() external view returns (uint8);
+    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool);
+    function getRoleAdmin(bytes32 role) external view returns (bytes32);
+    function grantRole(bytes32 role, address account) external;
+    function hasRole(bytes32 role, address account) external view returns (bool);
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
+    function mint(address to, uint256 amount) external;
+    function name() external view returns (string memory);
+    function nonces(address) external view returns (uint256);
+    function owner() external view returns (address);
+    function pendingOwner() external view returns (address);
+    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+        external;
+    function renounceRole(bytes32 role, address account) external;
+    function revokeRole(bytes32 role, address account) external;
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+    function symbol() external view returns (string memory);
+    function totalSupply() external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+    function transferOwnership(address newOwner) external;
+}
+
