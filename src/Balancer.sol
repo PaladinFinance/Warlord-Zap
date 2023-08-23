34e39d546fd84f211a1e2e37aaf22233b95c606c
Alberto <albicento.ac@gmail.com>
2023-08-23 14:07:15 +0200

    feat: Added `Balancer` and `Curve` placeholder

---
+++ src/Balancer.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+
+contract Balancer {
+  using SafeTransferLib for ERC20;
+
+  function _ethToAura(uint256 amount) internal {
+
+  }
+}
---
+++ src/Curve.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+
+contract Curve {
+  using SafeTransferLib for ERC20;
+
+  function _ethToCvx(uint256 amount) internal {
+
+  }
+}
