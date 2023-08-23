05b951513a7e7d07b7566bae47b7b2d263ef250b
Alberto <albicento.ac@gmail.com>
2023-08-23 18:05:52 +0200

    fix: Marking dex contracts as abstract

--- src/Balancer.sol
+++ src/Balancer.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
-contract Balancer {
+abstract contract Balancer {
     using SafeTransferLib for ERC20;
 
     function _ethToAura(uint256 amount) internal {}
--- src/Curve.sol
+++ src/Curve.sol
@@ -3,8 +3,8 @@ pragma solidity 0.8.20;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+abstract contract Curve {
 
-contract Curve {
     using SafeTransferLib for ERC20;
 
     function _ethToCvx(uint256 amount) internal {}
