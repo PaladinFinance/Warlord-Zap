75b78c9ed7f400cccc3d5711da334a5e64ee85b9
Alberto <albicento.ac@gmail.com>
2023-08-23 14:06:51 +0200

    feat: Minor changes in `Uniswap`

--- src/Uniswap.sol
+++ src/Uniswap.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.20;
-pragma abicoder v2;
 
 import {ISwapRouter} from "uniswap/ISwapRouter.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
@@ -9,7 +8,7 @@ import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 contract Uniswap {
     using SafeTransferLib for ERC20;
 
-    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     ISwapRouter public immutable swapRouter;
     uint24 public constant poolFee = 500;
@@ -18,15 +17,16 @@ contract Uniswap {
         swapRouter = _swapRouter;
     }
 
-    function _approveSwapper(address token) external {// TODO internalize 
+    function _approveSwapper(address token) internal {
         ERC20(token).safeApprove(address(swapRouter), type(uint256).max);
     }
 
+    // TODO add function to reset approval in case of hacks
     // TODO should the swap router have a setter?
     // TODO make setter for fee
 
-    function _etherize(address token, uint256 amountIn, uint256 etherOutMin, address receiver)
-        external //  TODO change this
+    function _etherize(address token, uint256 amountIn, uint256 etherOutMin)
+        internal 
         returns (uint256 amountOut)
     {
         // Pull the specified amount to this contract.
@@ -36,7 +36,7 @@ contract Uniswap {
             tokenIn: token, // The input token address
             tokenOut: WETH, // The token received should be Wrapped Ether
             fee: poolFee, // The fee tier of the pool
-            recipient: receiver, // Receiver of the swapped tokens
+            recipient: msg.sender, // Receiver of the swapped tokens
             deadline: block.timestamp, // Swap has to be terminated at block time
             amountIn: amountIn, // The exact amount to swap
             amountOutMinimum: etherOutMin, // Quote is given by frontend to ensure slippage is minimised
