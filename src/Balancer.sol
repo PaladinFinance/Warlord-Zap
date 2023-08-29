435f9a90b1c10be51a2c4b7311cb2ae40b17cadd
Alberto <albicento.ac@gmail.com>
2023-08-29 17:36:44 +0200

    style: Formatting

--- src/Balancer.sol
+++ src/Balancer.sol
@@ -14,28 +14,28 @@ abstract contract Balancer is EtherUtils {
     address vault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
 
     function resetBalancerAllowance() external {
-      _resetWethAllowance(vault);
+        _resetWethAllowance(vault);
     }
 
     function removeBalancerAllowance() external {
-      _removeWethAllowance(vault);
+        _removeWethAllowance(vault);
     }
 
     function _wethToAura(uint256 amount, uint256 auraOutMin) internal {
         IVault.SingleSwap memory params = IVault.SingleSwap({
-          poolId: 0xcfca23ca9ca720b6e98e3eb9b6aa0ffc4a5c08b9000200000000000000000274, // 50/50 WETH/AURA
-          kind: 0, // exact input, output given
-          assetIn: WETH, 
-          assetOut: AURA,
-          amount: amount, // Amount to swap
-          userData: ""
+            poolId: 0xcfca23ca9ca720b6e98e3eb9b6aa0ffc4a5c08b9000200000000000000000274, // 50/50 WETH/AURA
+            kind: 0, // exact input, output given
+            assetIn: WETH,
+            assetOut: AURA,
+            amount: amount, // Amount to swap
+            userData: ""
         });
 
         IVault.FundManagement memory funds = IVault.FundManagement({
-          sender: address(this), // Funds are taken from this contract
-          recipient: address(this), // Swapped tokens are sent back to this contract
-          fromInternalBalance: false, // Don't take funds from contract LPs (since there's none)
-          toInternalBalance: false // Don't LP with swapped funds
+            sender: address(this), // Funds are taken from this contract
+            recipient: address(this), // Swapped tokens are sent back to this contract
+            fromInternalBalance: false, // Don't take funds from contract LPs (since there's none)
+            toInternalBalance: false // Don't LP with swapped funds
         });
 
         IVault(vault).swap(params, funds, auraOutMin, type(uint256).max);
