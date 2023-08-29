2845f05c40ccb54815baaabd478a21a34c90784f
Alberto <albicento.ac@gmail.com>
2023-08-29 16:48:35 +0200

    feat: Added Curve allowance setters

--- src/Curve.sol
+++ src/Curve.sol
@@ -4,13 +4,20 @@ pragma solidity 0.8.20;
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {ICurvePool} from "curve/ICurvePool.sol";
+import {EtherUtils} from "src/EtherUtils.sol";
 
-abstract contract Curve {
-    // TODO handle weth redundancy
-    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+abstract contract Curve is EtherUtils {
+    using SafeTransferLib for ERC20;
+
     address private wethCvxPool = 0xB576491F1E6e5E62f1d8F26062Ee822B40B0E0d4;
 
-    using SafeTransferLib for ERC20;
+    function resetCurveAllowance() external {
+      _resetWethAllowance(wethCvxPool);
+    }
+
+    function removeCurveAllowance() external {
+      _removeWethAllowance(wethCvxPool);
+    }
 
     function _wethToCvx(uint256 amount, uint256 cvxOutMin) internal {
         ERC20(WETH).safeApprove(wethCvxPool, amount);
