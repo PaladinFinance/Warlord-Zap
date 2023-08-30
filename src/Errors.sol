a6691f18c32f5adbdef4bb0c0af66d9fe369e4bf
Alberto <albicento.ac@gmail.com>
2023-08-30 17:13:52 +0200

    style: Grouping errors

--- src/Errors.sol
+++ src/Errors.sol
@@ -2,8 +2,14 @@
 pragma solidity 0.8.20;
 
 library Errors {
+    // Generic errors
     error ZeroAddress();
     error NullAmount();
+
+    // Token management
     error TokenNotAllowed();
+    error TokenAlreadyAllowed();
+
+    // Mint ratios
     error InvalidRatio();
 }
