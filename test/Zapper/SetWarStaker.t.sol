dda74e6305e67517ba2b23d0db0ff3e19efc50c0
Alberto <albicento.ac@gmail.com>
2023-08-30 18:03:18 +0200

    test: Warlord setters

---
+++ test/Zapper/SetWarStaker.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import "./ZapperTest.sol";
+
+contract SetWarStaker is ZapperTest {
+    function test_defaultBehavior(address newWarStaker) public {
+        vm.expectEmit();
+        emit SetWarStaker(newWarStaker);
+
+        vm.prank(admin);
+        zap.setWarStaker(newWarStaker);
+
+        assertEq(zap.warStaker(), newWarStaker, "war staker should have been changed");
+    }
+
+    function test_onlyOnwer(address newWarStaker) public {
+        vm.expectRevert("Ownable: caller is not the owner");
+        zap.setWarStaker(newWarStaker);
+    }
+
+    function test_zeroAddress() public {
+        vm.expectRevert(Errors.ZeroAddress.selector);
+
+        vm.prank(admin);
+        zap.setWarStaker(address(0));
+    }
+}
