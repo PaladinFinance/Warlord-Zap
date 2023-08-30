00cd8adb835af4cbedb4de93cb87e90b5074d02f
Alberto <albicento.ac@gmail.com>
2023-08-30 18:03:18 +0200

    test: Warlord setters

---
+++ test/Zapper/SetWarMinter.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import "./ZapperTest.sol";
+
+contract SetWarMinter is ZapperTest {
+    function test_defaultBehavior(address newWarMinter) public {
+        vm.expectEmit();
+        emit SetWarMinter(newWarMinter);
+
+        vm.prank(admin);
+        zap.setWarMinter(newWarMinter);
+
+        assertEq(zap.warMinter(), newWarMinter, "war minter should have been changed");
+    }
+
+    function test_onlyOnwer(address newWarMinter) public {
+        vm.expectRevert("Ownable: caller is not the owner");
+        zap.setWarMinter(newWarMinter);
+    }
+
+    function test_zeroAddress() public {
+        vm.expectRevert(Errors.ZeroAddress.selector);
+
+        vm.prank(admin);
+        zap.setWarMinter(address(0));
+    }
+}
