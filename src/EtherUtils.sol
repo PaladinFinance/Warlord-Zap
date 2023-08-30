c80d230efce58c0cddcad920f297ce1a678e8229
Alberto <albicento.ac@gmail.com>
2023-08-30 13:39:58 +0200

    feat: Replace solady with oz

--- src/EtherUtils.sol
+++ src/EtherUtils.sol
@@ -3,9 +3,9 @@ pragma solidity 0.8.20;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
-import {Ownable} from "solady/auth/Ownable.sol";
+import {Ownable2Step} from "oz/access/Ownable2Step.sol";
 
-abstract contract EtherUtils is Ownable {
+abstract contract EtherUtils is Ownable2Step {
     using SafeTransferLib for ERC20;
 
     address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
