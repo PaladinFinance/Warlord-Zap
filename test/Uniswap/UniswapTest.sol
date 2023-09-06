350a65f22ff2d70b4ba18fa9e30f3e3a965824a5
Alberto <albicento.ac@gmail.com>
2023-09-06 20:15:47 +0200

    test(uni): `setUniswapRouter`

---
+++ test/Uniswap/UniswapTest.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import "test/MainnetTest.sol";
+import {Uniswap} from "src/Uniswap.sol";
+
+contract UniswapTest is MainnetTest {
+    using SafeTransferLib for ERC20;
+
+    UniswapMock uniswap;
+    address admin;
+
+    event SetUniswapRouter(address newRouter);
+
+    function setUp() public override {
+        MainnetTest.setUp();
+        admin = makeAddr("admin");
+        vm.prank(admin);
+        uniswap = new UniswapMock();
+    }
+}
+
+contract UniswapMock is Uniswap {
+    function etherize(address token, uint256 amountIn, uint256 ethOutMin, uint24 fee) external {
+        _etherize(token, amountIn, ethOutMin, fee);
+    }
+}
