ddbccb5b3050de539167b9b1c60708781217bdb4
Alberto <albicento.ac@gmail.com>
2023-09-07 18:00:36 +0200

    test: new boilerplate

--- test/MainnetTest.sol
+++ test/MainnetTest.sol
@@ -14,6 +14,8 @@ contract MainnetTest is Test {
     ERC20 public constant war = ERC20(0xa8258deE2a677874a48F5320670A869D74f0cbC1);
     ERC20 public constant weth = ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     ERC20 public constant usdc = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    ERC20 public constant usdt = ERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+    ERC20 public constant dai = ERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     ERC20 public constant cvx = ERC20(0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B);
     ERC20 public constant aura = ERC20(0xC0c293ce456fF0ED870ADd98a0828Dd4d2903DBF);
     IWarStaker public constant stkWar = IWarStaker(0xA86c53AF3aadF20bE5d7a8136ACfdbC4B074758A);
@@ -32,6 +34,8 @@ contract MainnetTest is Test {
         vm.label(address(war), "WAR");
         vm.label(address(stkWar), "stkWAR");
         vm.label(address(weth), "WETH");
+        vm.label(address(dai), "DAI");
+        vm.label(address(usdt), "USDT");
         vm.label(address(uniRouter), "Uniswap Router");
         vm.label(address(usdc), "USDC");
         vm.label(address(cvx), "CVX");
@@ -44,11 +48,3 @@ contract MainnetTest is Test {
 contract MockERC20 is ERC20 {
     constructor() ERC20("MockERC20", "MERC20", 18) {}
 }
-
-// contract BalancerMock is Balancer {
-// function wethToAura(uint256 amount, uint256 auraOutMin) external {
-// _wethToAura(amount, auraOutMin);
-// }
-// }
-// contract UniswapMock is Uniswap {}
-// contract CurveMock is Curve {}
