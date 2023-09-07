c741d8e79fd9a0337303f17632d9c0724896b042
Alberto <albicento.ac@gmail.com>
2023-09-07 18:00:44 +0200

    test: Testing etherize

--- test/Uniswap/Etherize.t.sol
+++ test/Uniswap/Etherize.t.sol
@@ -2,14 +2,40 @@
 pragma solidity 0.8.20;
 
 import "./UniswapTest.sol";
+import {OneInchQuotes} from "../OneInchQuotes.sol";
 
-contract Etherize is UniswapTest {
-    function setUp() public override {
+contract Etherize is UniswapTest, OneInchQuotes {
+    using SafeTransferLib for ERC20;
+
+    function setUp() public virtual override {
         blockNumber = 0;
         UniswapTest.setUp();
     }
 
-    function test_wip() public {
-        console2.log(block.number);
+    function assertSlippageLessThanOnePercent(address token, uint24 fee, uint256 amount, uint256 slippage_bps) public {
+        uniswap.resetUniswapAllowance(token);
+
+        deal(token, address(uniswap), amount);
+
+        // Fetching the best price from 1inch API
+        uint256 expectedAmount = fetchPrice(address(usdc), address(weth), amount);
+        // 1% slippage tollerated
+        uint256 slippedExpectedAmount = expectedAmount * (10_000 - slippage_bps) / 10_000;
+        uniswap.etherize(token, amount, slippedExpectedAmount, fee);
+        uint256 swappedAmount = weth.balanceOf(address(uniswap));
+
+        assertApproxEqRel(swappedAmount, expectedAmount, 0.01e18, "Slippage should be smaller than 1%");
+        assertEq(ERC20(token).balanceOf(address(uniswap)), 0, "after swap no token should be left in contract balance");
+    }
+
+    function test_stablecoinToEtherSlippage() public {
+        uint256 snapshot = vm.snapshot();
+        // Swap up to 1 milion usdc with a 0.05 fee
+        assertSlippageLessThanOnePercent(address(usdc), 500, 1_000_000e6, 100);
+        vm.revertTo(snapshot);
+        vm.sleep(10_000);
+        // Swap up to 1 milion usdt with a 0.05 fee
+        assertSlippageLessThanOnePercent(address(usdt), 500, 1_000_000e6, 100);
+        // Tried this with DAI but results were too inconsistent
     }
 }
